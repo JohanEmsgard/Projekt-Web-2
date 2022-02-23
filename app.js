347c09635fd46e296c5a7554ea0ba9771fdb8d6d
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2022-02-23 12:23:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+document.querySelector("#GB").addEventListener("change", changeGBPrice);
+
+function changeGBPrice(event) {
+    if (event.target.value === "128GB") {
+        document.querySelector(".price-container").innerHTML = "6643:- SEK";
+    }
+    if (event.target.value === "256GB") {
+        document.querySelector(".price-container").innerHTML = "7000:- SEK";
+    }
+}
